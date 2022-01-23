38da26936a9214f543169319c3f31bb770f5bfa1
Thibault Delaruelle <thibault.delaruelle@scriptandgo.com>
2022-01-23 20:31:03 +0100

    Ajout d'une classe Bus

--- WhereBus/src/app/app.module.ts
+++ WhereBus/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ WhereBus/src/app/bus.ts
@@ -0,0 +1,7 @@
+export interface bus {
+    numerobus : number;
+    sens : number;
+    nomcourtligne : String;
+    destination : String;
+    coordonees : number[];
+}
